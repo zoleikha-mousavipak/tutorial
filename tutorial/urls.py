bbef79a51b455cad8e342df406afb2f73cbd2234
zoleikha-mousavipak <zmpak2000@gmail.com>
2019-06-29 20:27:53 +0430

    new commit

---
+++ tutorial/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from tutorial.quickstart import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
+urlpatterns = [
+    path('' , include(router.urls)),
+    path('apt-auth/' , include('rest_framework.urls', namespace='rest_framework'))
+]
